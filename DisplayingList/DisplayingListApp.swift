414f4e4ce16db15a7034377e8a80f7f223501cfd
Jai <80166407+jairajput@users.noreply.github.com>
2023-07-01 16:41:08 +0530

    DisplyingList

---
+++ DisplayingList/DisplayingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayingListApp.swift
+//  DisplayingList
+//
+//  Created by Jai  on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DisplayingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
